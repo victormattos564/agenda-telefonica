b6f4aa80a4bde0d65710d59c2657ef762b58b2f7
iam.mattosz <victormaria@aluno.senai.br>
2023-09-04 08:40:50 -0300

    começando js

---
+++ assets/js/index.js
@@ -0,0 +1,14 @@
+class Agenda {
+    constructor(nome,tel,celular,URL,email,CEP,cidade,estado,github) {
+        this.nome = nome;
+        this.tel = tel;
+        this.celular = celular;
+        this.URL = URL;
+        this.email = email;
+        this.CEP = CEP;
+        this.cidade = cidade;
+        this.estado = estado;
+        this.github = github;
+        console.log("Passou pelo construtor da class Agenda")
+    }
+}
